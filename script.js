51f55df1108562d9d86223027d5ee2090754a177
lexinamer <lexinamer@gmail.com>
2015-01-29 08:34:37 -0500

    adding js files

---
+++ script.js
@@ -0,0 +1,23 @@
+//this file is a demo
+
+var userName = "Lexi";
+
+var todaysDate = new Date();
+
+
+function myFunction(){
+  document.write("Hello ");
+  document.write(userName);
+  console.log(todaysDate);
+
+}
+
+document.getElementById("demo").innerHTML="This is the new info";
+document.getElementById("demo2").innerHTML="This is something else";
+myFunction();
+
+var myNumberVar = 5;
+var myTextVar = "my Variable is a string";
+
+console.log(myNumberVar);
+console.log(myTextVar);
